80b4612b6e5ad101f3b028b6514874a72f435fef
pedrolopes10 <pedrolopes10@gmail.com>
2022-03-23 13:31:42 +0000

    try catch pathview

--- android/src/main/java/com/horcrux/svg/PathView.java
+++ android/src/main/java/com/horcrux/svg/PathView.java
@@ -17,26 +17,37 @@ import android.graphics.Path;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
 
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+
 @SuppressLint("ViewConstructor")
 class PathView extends RenderableView {
     private Path mPath;
+    private ReactContext reactContext;
 
     public PathView(ReactContext reactContext) {
         super(reactContext);
+        this.reactContext = reactContext;
         PathParser.mScale = mScale;
         mPath = new Path();
     }
 
     @ReactProp(name = "d")
     public void setD(String d) {
-        mPath = PathParser.parse(d);
-        elements = PathParser.elements;
-        invalidate();
+        try {
+             mPath = PathParser.parse(d);
+             elements = PathParser.elements;
+             invalidate();
+        } catch (Error e) {
+           WritableMap params = Arguments.createMap();
+           params.putString("error", e.getMessage());
+           this.reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("svgError", params);
+        }
     }
 
     @Override
     Path getPath(Canvas canvas, Paint paint) {
         return mPath;
     }
-
 }
