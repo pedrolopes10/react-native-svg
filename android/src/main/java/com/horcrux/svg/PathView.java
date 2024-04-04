53da9875690600dcd86c442144c4c6480c58d3aa
hvieira <hvieira@airnavsystems.com>
2024-04-04 21:54:15 +0100

    try catch pathview

--- android/src/main/java/com/horcrux/svg/PathView.java
+++ android/src/main/java/com/horcrux/svg/PathView.java
@@ -13,27 +13,40 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
 
 @SuppressLint("ViewConstructor")
 class PathView extends RenderableView {
   private Path mPath;
+  private ReactContext reactContext;
 
   public PathView(ReactContext reactContext) {
     super(reactContext);
+    this.reactContext = reactContext;
     PathParser.mScale = mScale;
     mPath = new Path();
   }
 
   public void setD(String d) {
-    mPath = PathParser.parse(d);
-    elements = PathParser.elements;
-    for (PathElement elem : elements) {
-      for (Point point : elem.points) {
-        point.x *= mScale;
-        point.y *= mScale;
+    try {
+      mPath = PathParser.parse(d);
+      elements = PathParser.elements;
+      for (PathElement elem : elements) {
+        for (Point point : elem.points) {
+          point.x *= mScale;
+          point.y *= mScale;
+        }
       }
+      invalidate();
+    } catch (Error e) {
+      WritableMap params = Arguments.createMap();
+      params.putString("error", e.getMessage());
+      this.reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("svgError", params);
     }
-    invalidate();
   }
 
   @Override
